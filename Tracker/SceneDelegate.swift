c9a437aa9618d335d396168381de94603c0c8215
Maxim <mquesmark@icloud.com>
2025-10-04 20:18:03 +0300

    Revert "Создан TabBar и начальный экран Трекеров"
    This reverts commit a0c87944fba80607d0d940b30fa6cc4d4255eb1c.

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -13,24 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let trackersVC = TrackersListViewController()
-        let trackersNavVC = UINavigationController(rootViewController: trackersVC)
-        trackersNavVC.tabBarItem = UITabBarItem(title: "Трекеры", image: UIImage(systemName: "record.circle"), tag: 0)
-        
-        let statisticsVC = StatisticsViewController()
-        let statisticsNavVC = UINavigationController(rootViewController: statisticsVC)
-        statisticsVC.tabBarItem = UITabBarItem(title: "Статистика", image: UIImage(systemName: "chart.bar"), tag: 1)
-        
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [trackersNavVC, statisticsNavVC]
-        
-        
-        window = UIWindow(windowScene: windowScene)
-        guard let window = window else { return }
-        window.rootViewController = tabBarController
-        window.makeKeyAndVisible()
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
