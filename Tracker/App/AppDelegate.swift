f94490fe9e34ec099ae891e5c72c493c564280d5
Maxim <mquesmark@icloud.com>
2025-11-06 13:45:16 +0300

    Добавлена база Метрики

--- Tracker/App/AppDelegate.swift
+++ Tracker/App/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AppMetricaCore
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,12 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         DaysValueTransformer.register()
+        if let configuration = AppMetricaConfiguration(apiKey: "9f81a850-e15b-4de6-88e5-cb195f5e2345") {
+            AppMetrica.activate(with: configuration)
+        } else {
+            print("WARNING: AppMetrica configuration is not valid")
+        }
+        
         return true
     }
 }
